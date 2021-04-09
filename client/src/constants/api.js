01c4b19f2d41fb0dff1b683a3fb391825614d836
lexz <nguyenqui1011@gmail.com>
2021-04-09 16:05:06 +0700

    fix api url

--- client/src/constants/api.js
+++ client/src/constants/api.js
@@ -1,6 +1,6 @@
 export const apiUrl =
   process.env.NODE_ENV !== "production"
     ? "http://localhost:5000/api"
-    : process.env.API_URL;
+    : "https://mern-learning-server.herokuapp.com/api";
 
 export const LOCAL_STORAGE_TOKEN_NAME = "userToken";
